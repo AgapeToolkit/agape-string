c497a270163ebebc19dba918c0b1d25bab4c9230
Maverik Minett <maverik@maverik.io>
2025-09-16 08:35:05 -0400

    add toPlural

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './lib/functions/toCamelCase';
 export * from './lib/functions/toKebabCase';
 export * from './lib/functions/toPascalCase';
 export * from './lib/functions/quantify';
+export * from './lib/functions/toPlural';
 export * from './lib/functions/toSingular';
 export * from './lib/functions/toSnakeCase';
 export * from './lib/functions/toTitleCase';
